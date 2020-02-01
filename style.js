c0f92bc25a38900b0c37838ca8a49eb676df4c90
varvadyk <39148487+varvadyk@users.noreply.github.com>
2020-02-01 16:02:08 +0200

    change  operations on branche and cycles

--- style.js
+++ style.js
@@ -1,26 +1,39 @@
-let n=prompt("Vvedit number");
+let n=prompt("Vvedit number N",10);
 console.log(n);
-console.log(Number(n));
-console.log(Number.isInteger(n));
-let m=prompt("Vvedit number");
+console.log(!Number.isInteger(n));
+let m=prompt("Vvedit number М ",100);
 console.log(m);
-console.log(Number.isInteger(m));
-if (confirm("Пропускати парне число")) {
-  alert("Потрібно");
+console.log(!Number.isInteger(m));
+let check=confirm("Пропускати парне число?");
+if (check===true) {
+  alert("Потрібно пропустити");
 } else {
-  alert("Не потрібно");
+  alert("Не потрібно пропускати ");
 
 
 }
+let result_numbers = 0;
 
-for(let  i=0; i===n && i===m; i++)
+for(let i=n; i<=m; i++)
 {
+  if (check===true) {
     if(i % 2 == 0)
-    {
+    { 
+      continue;
+      
         console.log("Парне число");
     }
+    result_numbers += i;
+  }
+
     else{
+      result_numbers += i;
   console.log ("не парне число")
-   let sum=n+m;
-   console.log("сума чисел"+sum)
-    }}
+}
+  
+}
+console.log ("Result:" + result_numbers);
+document.writeln(`<h1 style=color:red;>Цикли і розгалуження</h1>`);
+document.writeln(`Користувач ввів такі значення для N =${n} і M=${m}<br>`);
+document.writeln(`Сума чисел буде: ${result_numbers}`);
+
